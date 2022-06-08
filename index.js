d08b2c1287b4aa7a8909580f724752cee9d76e0e
Chew TingJun <swengineerwithisaac@gmail.com>
2022-06-08 18:13:01 +0800

    Updated comments

--- index.js
+++ index.js
@@ -7,45 +7,52 @@ const buttonColors = [
 ];
 
 let gamePattern = [];
-let gameStatus = false;
 let userClickedPattern = [];
-let level = 0;
+let gameStatus = false; // To track game status
+let level = 0 
 
 $(document).ready( function () {
     $(document).keypress( function () {
         if (!gameStatus) {
+            gameStatus = true;
             console.log(gamePattern);
             console.log(userClickedPattern);
+            // Start the game once key was pressed
             nextSequence();
-            gameStatus = true;
         }
     });
 });
 
+
 $("div.btn").click( function (e) {
-    let userChosenColor = e.currentTarget.id;
-    userClickedPattern.push(userChosenColor);
+    let userChosenColor = e.currentTarget.id; // Get the id by HTML object
+    userClickedPattern.push(userChosenColor); //Get what user have click 
     playSound(userChosenColor);
     animatePress(userChosenColor);
     checkAnswer(userClickedPattern.length - 1); // Get index
 });
+
 function nextSequence() {
+    // This function allow game random number and increase the level
+    // Set generated number animation  
     level++;
     $("#level-title").text(`Level ${level}`);
-    userClickedPattern = [];
+    userClickedPattern = []; //Empty user pattern
     let rand = Math.random();
-    let randomNumber = Math.floor((rand *= (3 + 1)));
+    let randomNumber = Math.floor((rand *= (3 + 1))); // Randomized
     let randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
+    gamePattern.push(randomChosenColor); // Set the default game pattern to compare later on
     $(`#${randomChosenColor}`).fadeOut().fadeIn();
 };
 
 function playSound(name) {
+    // This function will trigger sound if called
     var sounds = new Audio(`./sounds/${name}.mp3`);
     sounds.play();    
 }
 
 function animatePress(currentColor) {
+    // This function will trigger animation by adding class attr and removing it
     $(`#${currentColor}`).addClass("pressed");
     setTimeout(() => {
         $(`#${currentColor}`).removeClass("pressed");
@@ -53,6 +60,8 @@ function animatePress(currentColor) {
 }
 
 function checkAnswer (currentLevel) {
+    // This function its to check whether user click the correct sequence or not
+    // Since all the value will be correct then we only increase the level
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
         if (gamePattern.length === userClickedPattern.length) {
             setTimeout(() => {
@@ -60,6 +69,7 @@ function checkAnswer (currentLevel) {
             }, 1000);
         }
     } else {
+        // If user click the wrong sequence alert them then reset the game
         playSound("wrong");
         $("body").addClass("game-over");
         $("#level-title").text("Game Over, Press Any Key to Restart")
@@ -71,6 +81,8 @@ function checkAnswer (currentLevel) {
 }
 
 function startOver() {
+    // This function allows the game start over 
+    // It reset everything
     gamePattern = [];
     userClickedPattern = [];
     level = 0;
